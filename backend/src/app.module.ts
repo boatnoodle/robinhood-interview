1a16a07b6356bb81c8865adebad00f9ea3ffe8ff
boatnoodle <nattasit31@gmail.com>
2023-08-30 17:21:26 +0700

    update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CommentModule, InterviewModule } from './modules';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    InterviewModule,
+    CommentModule,
+  ],
 })
 export class AppModule {}
